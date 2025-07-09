8303c923bf58571cd00ee6dcb1da985a0f651cbd
Justohong <galactico.sj@gmail.com>
2025-07-09 10:14:50 +0900

    #5

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ManageGolf/',
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      minify: true,
-      inject: {
-        data: {
-          title: 'Golf Management',
-        },
-        tags: [
-          {
-            injectTo: 'body-prepend',
-            tag: 'div',
-            attrs: {
-              id: 'root',
-            },
-          },
-        ],
-      },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  plugins: [react()],
 })
